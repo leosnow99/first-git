3058908a92ca0aab98fcaae49c05e60452802906
leo <leosnow@126.com>
2019-12-25 09:58:50 +0800

    add new method

--- git-test/src/com/leo/FirstClass.java
+++ git-test/src/com/leo/FirstClass.java
@@ -3,5 +3,13 @@ package com.leo;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First Git project!");
+        System.out.println("hello");
+    }
+
+    public void test() {
+        int i = 10;
+        for (int i1 = 0; i1 < i; i1++) {
+            System.out.println(i1);
+        }
     }
 }
